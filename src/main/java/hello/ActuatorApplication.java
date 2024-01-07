6d7b92943f58cdf0121893534418518494077cb0
young970 <yyww9@naver.com>
2024-01-07 23:49:24 +0900

    [Feat]: logger 엔드포인트 추가, http 요청 이력 엔드포인트 추가.

--- src/main/java/hello/ActuatorApplication.java
+++ src/main/java/hello/ActuatorApplication.java
@@ -1,13 +1,21 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+    //자주 사용하진 않지만 개발 단계나 로컬에서 유용하게 쓰일수도 있다. (기능이 단순하고 재한이 많기 때문에 실제 운영환경에선 다른걸 쓰는걸 추천.)
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
     }
 
 }
